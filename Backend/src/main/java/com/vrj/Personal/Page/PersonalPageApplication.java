7fe20bae9c42947a2cbce6f6e48335139642b894
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-03 19:21:33 -0600

    Creation of initial project in spring boot with the dependencies: -Lombok -Spring Security -Spring Boot DevTools -Spring Web -Spring Data JPA - PostgreSQL Driver - Rest Repositories

---
+++ Backend/src/main/java/com/vrj/Personal/Page/PersonalPageApplication.java
@@ -0,0 +1,13 @@
+package com.vrj.Personal.Page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPageApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/vrj/Personal/Page/PersonalPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrj.Personal.Page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
