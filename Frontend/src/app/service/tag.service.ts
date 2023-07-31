1f2fcc9abca33c8922f1c14e7fa5443f949a36cf
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-30 23:12:46 -0600

    The interface and the Tag service have been added to make requests and receive a response

---
+++ Frontend/src/app/service/tag.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tag } from '../interface/tag';
+import { TAG_URL } from '../routes.constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService {
+
+  constructor(private http: HttpClient) { }
+
+  createTag(tag: Tag): Observable<any> {
+    return this.http.post(`${TAG_URL}/create`, tag);
+  }
+
+  updateTag(id: number, tag: Tag): Observable<any> {
+    return this.http.put(`${TAG_URL}/update/${id}`, tag);
+  }
+
+  deleteTag(id: number): Observable<any> {
+    return this.http.delete(`${TAG_URL}/delete/${id}`);
+  }
+}
