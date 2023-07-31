67eb86535ec009225f2a967a8dc089cf77e5b462
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-30 22:01:57 -0600

    A file with the constants corresponding to the request routes to the backend has been added.

---
+++ Frontend/src/app/routes.constants.ts
@@ -0,0 +1,16 @@
+/* It is the base url of the backend. */
+const BASEURL = 'http://localhost:8080';
+
+export const ABOUT_URL = BASEURL + '/about';
+export const ARTICLE_URL = BASEURL + '/articles';
+export const AUTHOR_URL = BASEURL + '/author';
+export const CERTIFICATION_URL = BASEURL + '/certifications';
+export const EDUCATION_URL = BASEURL + '/educations';
+export const IMAGE_URL = BASEURL + '/image';
+export const PERSONAL_project_URL = BASEURL + '/personal_projects';
+export const POST_URL = BASEURL + '/posts';
+export const REFERENCE_URL = BASEURL + '/references';
+export const SCHOLAR_PROJECT_URL = BASEURL + '/scholar_projects';
+export const TAG_URL = BASEURL + '/tag';
+export const USER_URL = BASEURL + '/users';
+export const WORK_EXPERIENCE_URL = BASEURL + '/work_experiences';
