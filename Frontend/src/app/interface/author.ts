d5e14d4e223baa5ec209d61bd3d05a6a14ef38c9
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-30 23:09:15 -0600

    The interface and the Author service have been added to make requests and receive a response

---
+++ Frontend/src/app/interface/author.ts
@@ -0,0 +1,7 @@
+export interface Author {
+    id: number;
+    name: string;
+    title: string;
+    url: string;
+    email: string;
+}
---
+++ Frontend/src/app/service/author.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Author } from '../interface/author';
+import { AUTHOR_URL } from '../routes.constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorService {
+
+  constructor(private http: HttpClient) { }
+
+  createAuthor(author: Author): Observable<any> {
+    return this.http.post(`${AUTHOR_URL}/create`, author);
+  }
+
+  updateAuthor(id: number, author: Author): Observable<any> {
+    return this.http.put(`${AUTHOR_URL}/update/${id}`, author);
+  }
+
+  deleteAuthor(id: number): Observable<any> {
+    return this.http.delete(`${AUTHOR_URL}/delete/${id}`);
+  }
+}
